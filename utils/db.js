df2ed6bd0b52f382972586ba0c9998e2ffad9b55
AARAMBH12 <himanshusharma70370749@gmail.com>
2025-04-12 03:54:41 +1300

    Added

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // Connect to MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/JobPortal");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected...");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
